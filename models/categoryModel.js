a72486e0ecdb5ebb0d20a6e8a90000c4deda753e
Tiến Trịnh <tientq.dev.it@gmail.com>
2025-06-04 17:40:48 +0700

    fix __v không update với thay đổi nhỏ
    thêm thuộc tính optimisticConcurrency

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     name: { type: String, required: true },
     parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', default: null }, // null nếu là cấp 1
-});
+}, { optimisticConcurrency: true });
 
 module.exports = mongoose.model('Category', categorySchema);
