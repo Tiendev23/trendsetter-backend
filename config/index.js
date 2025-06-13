c49f3b5b34ffff74d27ca85d02fa7d8bd04d7f1a
Tiến Trịnh <tientq.dev.it@gmail.com>
2025-06-13 16:49:42 +0700

    feature cloudinary

--- config/index.js
+++ config/index.js
@@ -1,5 +1,14 @@
 require('dotenv').config();
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+    cloud_name: "trendsetter",
+    api_key: "392848611572862",
+    api_secret: "iMMifNcnulZ2IWNAh_ms3ZcHzss"
+});
+
 module.exports = {
     SECRET_KEY: process.env.SECRET_KEY,
     MONGO_URI: process.env.MONGO_URI,
+    cloudinary,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,16 +4,17 @@ const productController = require('../controllers/productController');
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
-});
-const upload = multer({ storage });
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads/');
+//     },
+//     filename: function (req, file, cb) {
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//         cb(null, uniqueSuffix + path.extname(file.originalname));
+//     }
+// });
+// const upload = multer({ storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/', productController.getAllProducts);
 router.delete('/:id', productController.deleteProduct);
