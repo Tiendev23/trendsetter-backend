0678d40398ea6a491b4dff9a2b455ff7b68e0097
Tiến Trịnh <tientq.dev.it@gmail.com>
2025-06-22 19:02:08 +0700

    update config cloudinary

--- config/index.js
+++ config/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const cloudinary = require("cloudinary").v2
 
-const cloudinary = require("cloudinary").v2.config({
-    cloud_name: process.env.CLOUDINARY_URL.split('@')[1],
-    api_key: process.env.CLOUDINARY_URL.split('//')[1].split(':')[0],
-    api_secret: process.env.CLOUDINARY_URL.split('//')[1].split(':')[1].split('@')[0]
+cloudinary.config({
+    // cloud_name: process.env.CLOUDINARY_URL.split('@')[1],
+    // api_key: process.env.CLOUDINARY_URL.split('//')[1].split(':')[0],
+    // api_secret: process.env.CLOUDINARY_URL.split('//')[1].split(':')[1].split('@')[0]
+    secure: true,
 });
 
 const payosInstance = require('axios').create({
